fd5e8921df21d26b438fef07cd9cd791762f6d7f
Kaio Delphino <github@kaiodelphino.com>
2023-07-24 14:57:15 -0300

    feat: add one more harpoon slot

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -5,10 +5,12 @@ local config = function()
   vim.keymap.set("n", "<leader>ha", mark.add_file)
   vim.keymap.set("n", "<leader>hh", ui.toggle_quick_menu)
 
-  vim.keymap.set("n", "<C-m>", function() ui.nav_file(1) end)
-  vim.keymap.set("n", "<C-,>", function() ui.nav_file(2) end)
-  vim.keymap.set("n", "<C-.>", function() ui.nav_file(3) end)
-  vim.keymap.set("n", "<C-/>", function() ui.nav_file(4) end)
+  -- vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
+  vim.keymap.set("n", "<C-n>", function() ui.nav_file(1) end)
+  vim.keymap.set("n", "<C-m>", function() ui.nav_file(2) end)
+  vim.keymap.set("n", "<C-,>", function() ui.nav_file(3) end)
+  vim.keymap.set("n", "<C-.>", function() ui.nav_file(4) end)
+  vim.keymap.set("n", "<C-/>", function() ui.nav_file(5) end)
 end
 
 return {
