4b244d88f191e4bffcc2a84c767c4b2284792876
Kaio Delphino <github@kaiodelphino.com>
2023-07-29 11:40:03 -0300

    feat: update harpoon keybindings

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -6,7 +6,7 @@ local config = function()
   vim.keymap.set("n", "<leader>hh", ui.toggle_quick_menu)
 
   -- vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-  vim.keymap.set("n", "<C-n>", function() ui.nav_file(1) end)
+  vim.keymap.set("n", "<C-'>", function() ui.nav_file(1) end)
   vim.keymap.set("n", "<C-m>", function() ui.nav_file(2) end)
   vim.keymap.set("n", "<C-,>", function() ui.nav_file(3) end)
   vim.keymap.set("n", "<C-.>", function() ui.nav_file(4) end)
