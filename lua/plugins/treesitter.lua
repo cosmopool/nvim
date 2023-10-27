b3df00cd295d061aa1c38f82addbec3c2a0c1bea
Kaio Delphino <github@kaiodelphino.com>
2023-10-27 11:40:45 -0300

    chore: code style

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,24 @@
+local opts = {
+  ensure_installed = {
+    "lua",
+    "dart",
+    "go",
+    "json",
+    "yaml",
+    "v",
+    "python",
+    "swift",
+    "c",
+    "rust",
+    "markdown_inline",
+    "markdown",
+  },
+}
+
 return {
   "nvim-treesitter/nvim-treesitter",
   tag = "v0.9.0",
   lazy = false,
   priority = 1000,
-  opts = {
-    ensure_installed = { "lua", "dart", "go", "json", "yaml", "v", "python", "swift", "c", "rust", "markdown_inline",
-      "markdown", },
-  },
+  opts = opts,
 }
