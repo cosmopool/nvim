51603981f8fca2215c25130f2ff038408c20b5ee
Kaio Delphino <github@kaiodelphino.com>
2023-10-27 14:19:50 -0300

    refac: leave old closing tags of flutter-tools

--- lua/plugins/flutter-tools.lua
+++ lua/plugins/flutter-tools.lua
@@ -26,11 +26,11 @@ local setup = {
   widget_guides = {
     enabled = false,
   },
-  closing_tags = {
-    highlight = "ErrorMsg", -- highlight for the closing tag
-    prefix = ">",           -- character to use for close tag e.g. > Widget
-    enabled = true          -- set to false to disable
-  },
+  -- closing_tags = {
+  --   highlight = "ErrorMsg", -- highlight for the closing tag
+  --   prefix = ">",           -- character to use for close tag e.g. > Widget
+  --   enabled = true          -- set to false to disable
+  -- },
   lsp = {
     color = {
       enabled = true,
