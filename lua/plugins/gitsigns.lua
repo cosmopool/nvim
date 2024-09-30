660591da8ddbaf3b5d71633cc4519a91cd15f762
Kaio Delphino <dash.zet@gmail.com>
2024-09-30 11:13:52 -0300

    feat: setup gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,10 +1,7 @@
-local get_icon = require("core.utils").get_icon
+local function config()
+  local get_icon = require("core.utils").get_icon
 
-return {
-  "lewis6991/gitsigns.nvim",
-  enabled = vim.fn.executable "git" == 1,
-  -- event = "User AstroGitFile",
-  opts = {
+  local setup = {
     signs = {
       add = { text = get_icon "GitSign" },
       change = { text = get_icon "GitSign" },
@@ -13,6 +10,27 @@ return {
       changedelete = { text = get_icon "GitSign" },
       untracked = { text = get_icon "GitSign" },
     },
+    auto_attach = true,
+    current_line_blame = false,
+    current_line_blame_opts = {
+      virt_text = true,
+      virt_text_pos = 'eol',
+      delay = 300,
+      ignore_whitespace = true,
+      virt_text_priority = 100,
+      use_focus = true,
+    },
+  }
+
+  require("gitsigns").setup(setup)
+end
+
+return {
+  "lewis6991/gitsigns.nvim",
+  tag = "v0.9.0",
+  enabled = vim.fn.executable "git" == 1,
+  config = config,
+  opts = {
     worktrees = vim.g.git_worktrees,
   },
 }
