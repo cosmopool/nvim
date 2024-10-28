7dea8b8d5e72779c4ad1abd01b6ffd48554e05a4
Kaio Delphino <dash.zet@gmail.com>
2024-10-28 17:51:23 -0300

    feat: open dapui on debugger init

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -112,7 +112,28 @@ return {
     dependencies = { "nvim-dap", "folke/neodev.nvim" },
     -- tag = "v4.0.0",
     commit = "a5606bc5958db86f8d92803bea7400ee26a8d7e4",
-    config = function() require("dapui").setup() end,
+    config = function()
+      local imported, dapui = pcall(require, "dapui")
+      if not imported then
+        return
+      end
+
+      local imported, dap = pcall(require, "dapui")
+      if not imported then
+        return
+      end
+
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.after.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.after.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
   },
   {
     "nvim-neotest/nvim-nio",
