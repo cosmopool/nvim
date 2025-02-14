419085aefe0b2e621f247ca5d1b7388b46721803
Kaio Delphino <dash.zet@gmail.com>
2025-02-14 11:29:34 -0300

    feat: config no-neck-pain

--- lua/plugins/no-neck-pain.lua
+++ lua/plugins/no-neck-pain.lua
@@ -1,4 +1,17 @@
+local config = {
+  width = 120,
+  integrations = {
+    NeoTree = {
+      position = "right",
+      reopen = true,
+    },
+  }
+}
+
 return {
   "shortcuts/no-neck-pain.nvim",
   tag = "v2.1.5",
+  config = function()
+    require("no-neck-pain").setup(config)
+  end,
 }
