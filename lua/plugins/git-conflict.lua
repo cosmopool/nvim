26f18d475a071636a2252a95e018e245832398d0
Kaio Delphino <dash.zet@gmail.com>
2025-02-24 20:58:30 -0300

    chore: make plugin mappings more apparent

--- lua/plugins/git-conflict.lua
+++ lua/plugins/git-conflict.lua
@@ -1,10 +1,13 @@
--- DEFAULT MAPPINGS
+-- ========================================================
+--                    DEFAULT MAPPINGS
+-- ========================================================
 -- co — choose ours
 -- ct — choose theirs
 -- cb — choose both
 -- c0 — choose none
 -- ]x — move to previous conflict
 -- [x — move to next conflict
+-- ========================================================
 
 
 return {
