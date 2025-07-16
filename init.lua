7fa0ca0378679040124112ef06668116d0d75389
Kaio Delphino <dash.zet@gmail.com>
2025-07-16 14:20:33 -0300

    add color and icons on dap breakpoint lines

--- init.lua
+++ init.lua
@@ -13,3 +13,20 @@ require("core.keymaps")
 -- THEME ------------------------------
 vim.cmd.colorscheme("nordic")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "DapBreakpointFG", { fg = "#D90000", bg = "#471615" })
+vim.api.nvim_set_hl(0, "DapBreakpoint", { bg = "#471615" })
+vim.api.nvim_set_hl(0, "DapStopped", { bg = "#154729" })
+
+vim.fn.sign_define("DapBreakpoint", {
+  text = "",
+  texthl = "DapBreakpointFG",
+  linehl = "DapBreakpoint",
+  numhl =
+  "DapBreakpoint"
+})
+vim.fn.sign_define("DapBreakpointCondition",
+  { text = "ﳁ", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" })
+vim.fn.sign_define("DapBreakpointRejected",
+  { text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" })
+vim.fn.sign_define("DapLogPoint", { text = "", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" })
+vim.fn.sign_define("DapStopped", { text = "", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" })
